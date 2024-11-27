89c5ef6ea49041642e9a78eb9a98783bbbddafbd
THEO CARRETTE <theo.carrette@nomachine.univ-eiffel.fr>
2024-11-27 15:03:54 +0100

    Finish TP2

--- TP2_GPGPU/src/student.cu
+++ TP2_GPGPU/src/student.cu
@@ -121,17 +121,77 @@ namespace IMAC
 						int dX = x + i - matSize / 2;
 						int dY = y + j - matSize / 2;
 
-						// Handle out-of-bounds
-						if (dX < 0) dX = 0;
-						if (dX >= width) dX = width - 1;
-						if (dY < 0) dY = 0;
-						if (dY >= height) dY = height - 1;
+						// Handle borders
+						if ( dX < 0 ) 
+							dX = 0;
+
+						if ( dX >= width ) 
+							dX = width - 1;
+
+						if ( dY < 0 ) 
+							dY = 0;
+
+						if ( dY >= height ) 
+							dY = height - 1;
 
 						// Compute normalized coordinates
-						float texCoord = (dY * width + dX) / (float)(width * height);
+						float texCoord = dY * width + dX;
 
 						// Fetch from texture
 						uchar4 pixel = tex1Dfetch<uchar4>(texObj, texCoord);
+						
+						const int idMat = j * matSize + i;
+						sum.x += (float)pixel.x * constant_mat[idMat];
+						sum.y += (float)pixel.y * constant_mat[idMat];
+						sum.z += (float)pixel.z * constant_mat[idMat];
+
+						/*const int idPixel	= dY * width + dX;
+						sum.x += (float)input[idPixel].x * constant_mat[idMat];
+						sum.y += (float)input[idPixel].y * constant_mat[idMat];
+						sum.z += (float)input[idPixel].z * constant_mat[idMat];*/
+					}
+				}
+				const int idOut = y * width + x;
+				output[idOut].x = (uchar)clampf_dev( sum.x, 0.f, 255.f );
+				output[idOut].y = (uchar)clampf_dev( sum.y, 0.f, 255.f );
+				output[idOut].z = (uchar)clampf_dev( sum.z, 0.f, 255.f );
+				output[idOut].w = 255;
+			}
+		}
+	}
+
+	__global__
+	void conv_2dtex(cudaTextureObject_t texObj, const uint width, const uint height, const uint matSize, uchar4 *const output) {
+		for(uint y = blockIdx.y * blockDim.y + threadIdx.y; y < height; y += gridDim.y * blockDim.y) {
+			for(uint x = blockIdx.x * blockDim.x + threadIdx.x; x < width; x += gridDim.x * blockDim.x) {
+				float3 sum = make_float3(0.f,0.f,0.f);
+				
+				// Apply convolution
+				for ( uint j = 0; j < matSize; ++j ) 
+				{
+					for ( uint i = 0; i < matSize; ++i ) 
+					{
+						int dX = x + i - matSize / 2;
+						int dY = y + j - matSize / 2;
+
+						// Handle borders
+						if ( dX < 0 ) 
+							dX = 0;
+
+						if ( dX >= width ) 
+							dX = width - 1;
+
+						if ( dY < 0 ) 
+							dY = 0;
+
+						if ( dY >= height ) 
+							dY = height - 1;
+
+						// Compute normalized coordinates
+						int texCoord = dY * width + dX;
+
+						// Fetch from texture
+						uchar4 pixel = tex2D<uchar4>(texObj, texCoord % width, texCoord / height);
 						
 						const int idMat = j * matSize + i;
 						sum.x += (float)pixel.x * constant_mat[idMat];
@@ -229,7 +289,6 @@ namespace IMAC
 		chrGPU.stop();
 		std::cout 	<< "-> Done : " << chrGPU.elapsedTime() << " ms" << std::endl << std::endl;
 
-		cudaDeviceProp prop;
 		const dim3 threads(32, 32); // 32 * 32 = 1024
 		const dim3 blocks( ( imgWidth + threads.x - 1 ) / threads.x, ( imgHeight + threads.y - 1 ) / threads.y );
 
@@ -298,14 +357,13 @@ namespace IMAC
 		chrGPU.stop();
 		std::cout 	<< "-> Done : " << chrGPU.elapsedTime() << " ms" << std::endl << std::endl;
 
-		cudaDeviceProp prop;
 		const dim3 threads(32, 32); // 32 * 32 = 1024
 		const dim3 blocks( ( imgWidth + threads.x - 1 ) / threads.x, ( imgHeight + threads.y - 1 ) / threads.y );
 
 		std::cout << "-> Size of blocks x : " << blocks.x << " || Size of blocks y : " << blocks.y << std::endl;
 		
 		// Launch kernel
-		std::cout << "Naive Convolution on GPU (" 	<< blocks.x << "x" << blocks.y << " blocks - " 
+		std::cout << "Constant Convolution on GPU (" 	<< blocks.x << "x" << blocks.y << " blocks - " 
 												<< threads.x << "x" << threads.y << " threads)" << std::endl;
 		chrGPU.start();
 		conv_const<<<blocks, threads>>>(dev_input, imgWidth, imgHeight, matSize, dev_output);
@@ -369,36 +427,33 @@ namespace IMAC
 
 		// Create CUDA array for texture binding
 		cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<uchar4>();
-		cudaArray_t dev_array;
-		cudaMallocArray(&dev_array, &channelDesc, inputImg.size());
-
-		// Copy device memory to CUDA array
-		cudaMemcpyToArray(dev_array, 0, 0, inputImg.data(), bytes, cudaMemcpyDeviceToDevice);
 
 		// Init texture ressources
 		struct cudaResourceDesc resDesc {};
-		resDesc.resType = cudaResourceTypeArray;
-		resDesc.res.array.array = dev_array;
+		resDesc.resType = cudaResourceTypeLinear;
+		resDesc.res.linear.devPtr = dev_input;
+		resDesc.res.linear.sizeInBytes = bytes;
+		resDesc.res.linear.desc = channelDesc;
 
 		// Init texture descriptor
 		struct cudaTextureDesc texDesc {};
 		texDesc.addressMode[0] = cudaAddressModeClamp;
+		texDesc.addressMode[1] = cudaAddressModeClamp;
 		texDesc.filterMode = cudaFilterModePoint;
 		texDesc.readMode = cudaReadModeElementType;
-		texDesc.normalizedCoords = 1;
+		//texDesc.normalizedCoords = 0;
 
 		// Init texture object
 		cudaTextureObject_t texObj = 0;
 		cudaCreateTextureObject(&texObj, &resDesc, &texDesc, nullptr);
 
-		cudaDeviceProp prop;
 		const dim3 threads(32, 32); // 32 * 32 = 1024
 		const dim3 blocks( ( imgWidth + threads.x - 1 ) / threads.x, ( imgHeight + threads.y - 1 ) / threads.y );
 
 		std::cout << "-> Size of blocks x : " << blocks.x << " || Size of blocks y : " << blocks.y << std::endl;
 		
 		// Launch kernel
-		std::cout << "Naive Convolution on GPU (" 	<< blocks.x << "x" << blocks.y << " blocks - " 
+		std::cout << "Tex1D Convolution on GPU (" 	<< blocks.x << "x" << blocks.y << " blocks - " 
 												<< threads.x << "x" << threads.y << " threads)" << std::endl;
 		chrGPU.start();
 		conv_1dtex<<<blocks, threads>>>(texObj, imgWidth, imgHeight, matSize, dev_output);
@@ -465,43 +520,47 @@ namespace IMAC
 
 		// Create CUDA array for texture binding
 		cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<uchar4>();
-		cudaArray_t dev_array;
-		cudaMallocArray(&dev_array, &channelDesc, inputImg.size());
-		//cudaMallocPitch()
+		uchar4 *dev_array = NULL;
+		size_t pitch;
+		//cudaMallocArray(&dev_array, &channelDesc, bytes);
+		cudaMallocPitch(&dev_array, &pitch, imgWidth * sizeof(uchar4), imgHeight);
 
 		// Copy device memory to CUDA array
-		cudaMemcpyToArray(dev_array, 0, 0, dev_input, bytes, cudaMemcpyDeviceToDevice);
+		//cudaMemcpyToArray(dev_array, 0, 0, dev_input, bytes, cudaMemcpyDeviceToDevice);
+		cudaMemcpy2D(dev_array, pitch, inputImg.data(), imgWidth * sizeof(uchar4), imgWidth * sizeof(uchar4), imgHeight, cudaMemcpyHostToDevice);
 
 		// Free the device buffer because data is now in CUDA Array
 		cudaFree(dev_input);
 
 		// Init texture ressources
 		struct cudaResourceDesc resDesc {};
-		resDesc.resType = cudaResourceTypeArray;
-		resDesc.res.array.array = dev_array;
+		resDesc.resType = cudaResourceTypePitch2D;
+		resDesc.res.pitch2D.desc = cudaCreateChannelDesc<uchar4>();
+		resDesc.res.pitch2D.devPtr = dev_array;
+		resDesc.res.pitch2D.height = imgHeight;
+		resDesc.res.pitch2D.width = imgWidth;
+		resDesc.res.pitch2D.pitchInBytes = pitch;
 
 		// Init texture descriptor
 		struct cudaTextureDesc texDesc {};
 		texDesc.addressMode[0] = cudaAddressModeClamp;
 		texDesc.filterMode = cudaFilterModePoint;
 		texDesc.readMode = cudaReadModeElementType;
-		texDesc.normalizedCoords = 1;
 
 		// Init texture object
 		cudaTextureObject_t texObj = 0;
 		cudaCreateTextureObject(&texObj, &resDesc, &texDesc, nullptr);
 
-		cudaDeviceProp prop;
 		const dim3 threads(32, 32); // 32 * 32 = 1024
 		const dim3 blocks( ( imgWidth + threads.x - 1 ) / threads.x, ( imgHeight + threads.y - 1 ) / threads.y );
 
 		std::cout << "-> Size of blocks x : " << blocks.x << " || Size of blocks y : " << blocks.y << std::endl;
 		
 		// Launch kernel
-		std::cout << "Naive Convolution on GPU (" 	<< blocks.x << "x" << blocks.y << " blocks - " 
+		std::cout << "Tex2D Convolution on GPU (" 	<< blocks.x << "x" << blocks.y << " blocks - " 
 												<< threads.x << "x" << threads.y << " threads)" << std::endl;
 		chrGPU.start();
-		conv_const<<<blocks, threads>>>(dev_input, imgWidth, imgHeight, matSize, dev_output);
+		conv_2dtex<<<blocks, threads>>>(texObj, imgWidth, imgHeight, matSize, dev_output);
 		chrGPU.stop();
 		std::cout 	<< "-> Done : " << chrGPU.elapsedTime() << " ms" << std::endl << std::endl;
 
@@ -534,6 +593,6 @@ namespace IMAC
 		studentJob_Naive(inputImg,imgWidth,imgHeight,matConv,matSize,resultCPU,output);
 		studentJob_Constant(inputImg,imgWidth,imgHeight,matConv,matSize,resultCPU,output);
 		studentJob_Tex1D(inputImg,imgWidth,imgHeight,matConv,matSize,resultCPU,output);
-		//studentJob_Tex2D(inputImg,imgWidth,imgHeight,matConv,matSize,resultCPU,output);		
+		studentJob_Tex2D(inputImg,imgWidth,imgHeight,matConv,matSize,resultCPU,output);		
 	}
 }
